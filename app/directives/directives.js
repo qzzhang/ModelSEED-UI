062f0aa9044989fe6b2462882686c244b393ecd7
qzzhang <qzhang@anl.gov>
2021-02-24 18:15:10 -0600

    linking feature's orthologs to protein family tree

--- app/directives/directives.js
+++ app/directives/directives.js
@@ -1639,7 +1639,7 @@ function($compile, $stateParams) {
                 if (scope.isDemo == "true")
                     loadPhyloXML_demo(tree_name, func_name, col_id, ev);
                 else
-                    loadPhyloXML(tree_name, func_name, col_id, ev);
+                    loadPhyloXML(tree_name, func_name, col_id, gene_data, ev);
 
                 ev.stopPropagation();
                 ev.preventDefault();
@@ -1682,7 +1682,7 @@ function($compile, $stateParams) {
                 scope.errLoadingFamTree = '';
             }
 
-            function loadPhyloXML(treeName, func_name, col_id, ev) {
+            function loadPhyloXML(treeName, func_name, col_id, gene_data, ev) {
                 // loading the family tree data (in extendable phyloxml format)
                 scope.loadingFamTree = true;
                 var phyloxmlDoc = null;
@@ -1691,41 +1691,35 @@ function($compile, $stateParams) {
 
                 WS.get(phyloxml_wsPath)
                 .then(function(res) {
-                    if (res == undefined) {
+                    if (res == undefined || res.data == undefined) {
                         scope.treeData = null;
                         scope.loadingFamTreeFailed = true;
                         scope.errLoadingFamTree = 'No family tree data found!';
                     }
                     else {
                         var xml_str = res.data;
-                        if (xml_str == undefined) {
-                            scope.treeData = null;
-                            scope.loadingFamTreeFailed = true;
-                            scope.errLoadingFamTree = 'No family tree data found!';
-                        }
-                        else {
-                            // DOMParser parses an xml string into a DOM tree and return a XMLDocument in memory
-                            // XMLSerializer will do the reverse of DOMParser
-                            // var oSerializer = new XMLSerializer();
-                            // var sXML = oSerializer.serializeToString(xmldoc);
-                            var p = new DOMParser();
-                            phyloxmlDoc = p.parseFromString(xml_str, 'application/xml');
-                            console.log(phyloxmlDoc.documentElement.nodeName == "parsererror" ? "error while parsing"
-                                        : phyloxmlDoc.documentElement.nodeName);
-                            scope.treeData = phyloxmlDoc;
 
-                            var oSerializer = new XMLSerializer();
-                            updateAnnotationInTree(func_name, col_id)
-                            .then(function(xmldoc) {
-                                scope.xmldoc = xmldoc;
-                                scope.sXML = oSerializer.serializeToString(xmldoc);
-                                fetchDownloadURL(ev, treeName, func_name, col_id);
-                                ev.stopPropagation();
-                                ev.preventDefault();
-                                scope.loadingFamTreeFailed = false;
-                                scope.errLoadingFamTree = '';
-                            })
-                        }
+                        // DOMParser parses an xml string into a DOM tree and return a XMLDocument in memory
+                        // XMLSerializer will do the reverse of DOMParser
+                        // var oSerializer = new XMLSerializer();
+                        // var sXML = oSerializer.serializeToString(xmldoc);
+                        var p = new DOMParser();
+                        phyloxmlDoc = p.parseFromString(xml_str, 'application/xml');
+                        console.log(phyloxmlDoc.documentElement.nodeName == "parsererror" ? "error while parsing"
+                                    : phyloxmlDoc.documentElement.nodeName);
+                        scope.treeData = phyloxmlDoc;
+
+                        var oSerializer = new XMLSerializer();
+                        updateAnnotationInTree(func_name, col_id, gene_data)
+                        .then(function(xmldoc) {
+                            scope.xmldoc = xmldoc;
+                            scope.sXML = oSerializer.serializeToString(xmldoc);
+                            fetchDownloadURL(ev, treeName, func_name, col_id);
+                            ev.stopPropagation();
+                            ev.preventDefault();
+                            scope.loadingFamTreeFailed = false;
+                            scope.errLoadingFamTree = '';
+                        })
                     }
                     scope.loadingFamTree = false;
                 })
@@ -1867,26 +1861,11 @@ function($compile, $stateParams) {
             }
 
             // Modify the XML data structure according to the annotations in column (col_id)
-            var updateAnnotationInTree = function(func, col_id) {
+            var updateAnnotationInTree = function(func, col_id, gene_data) {
+                // adding score and ortholog matches to the xmldoc
                 var xmldoc = scope.treeData.cloneNode(true);
-                var dKeys = Object.keys(scope.dataClone);
+                xmldoc = mapAnnotations(xmldoc, 'name', gene_data['orthologs']);
 
-                // convert html DOM dropdown box options into three object arrays
-                for (var r=1; r<dKeys.length-1; r++) { // r=1 skip header
-                    var row_col = 'row' + r.toString() + '_col' + col_id.toString();
-                    var can_id = 'can_' + row_col, cur_id = 'cur_' + row_col, pre_id = 'pre_' + row_col;
-                    var can_arr = [], cur_arr = [], pre_arr = [];
-                    if (document.getElementById(can_id) !== null)
-                        can_arr = document.getElementById(can_id).options;
-                    if (document.getElementById(cur_id) !== null)
-                        cur_arr = document.getElementById(cur_id).options;
-                    if (document.getElementById(pre_id) !== null)
-                        pre_arr = document.getElementById(pre_id).options;
-
-                    // map the above object arrays with the xmldoc nodes
-                    if (can_arr != [])
-                        xmldoc = mapAnnotations(xmldoc, 'name', can_arr, cur_arr, pre_arr);
-                }
                 // After all annotations have been updated with new xml nodes added, append the last:
                 var root_node = xmldoc.getElementsByTagName('phyloxml')[0];
                 var lbls = xmldoc.createElement('labels', root_node.namespaceURI);
@@ -1919,42 +1898,47 @@ function($compile, $stateParams) {
                 return Promise.resolve(xmldoc);
             }
 
-            // run through the annotation arrays to find gene_name matches
-            // looping through these three arrays--if gene is in cur_arr, then green color
-            // will be assigned to that 'clade' node; if gene is in pre_arr, then blue
-            // color will be assigned to that 'clade' node; if gene is not in either
-            // cur_arr or pre_arr, red color will be assigned to that 'clade' node;
-            // otherwise, black color will be assigned to that 'clade' node.
-            function mapAnnotations(xmldoc, tagName, can_arr, cur_arr, pre_arr) {
+            function get_rgb(minimum, maximum, value) {
+                var minimum = parseFloat(minimum),
+                    maximum = parseFloat(maximum),
+                    value = parseFloat(value);
+                var ratio = 2 * (value - minimum) / (maximum - minimum);
+                r = parseInt(Math.max(0, 255*(1 - ratio)), 10);
+                g = parseInt(Math.max(0, 255*(ratio - 1)), 10);
+                b = 255 - g - r;
+                return [r, g, b];
+            }
+
+            // run through the tree nodes to find ortholog matches in ortho_data
+            // looping through the gene's orthologs and matching them with the family tree node names;
+            // assign a color to each of the matched tree node with a color range
+            // from deep red at 25% to green at 75%, i.e., from rgb(255, 0, 0) to rgb(0, 255, 0)
+            // adding the numberical score number beside the color box.
+            function mapAnnotations(xmldoc, tagName, ortho_data) {
                 // get the `phylogeny` node as the root
                 var tree = xmldoc.firstChild.childNodes[0];
                 var namespc = tree.namespaceURI;
                 if (tree.childElementCount > 0) {
-                    var can_genes = [], cur_genes = [], pre_genes = [];
-                    for (var k1=0; k1<can_arr.length; k1++) {
-                        can_genes[k1] = can_arr[k1].text;
-                    }
-                    for (var k2=0; k2<cur_arr.length; k2++) {
-                        cur_genes[k2] = cur_arr[k2].text;
-                    }
-                    for (var k3=0; k3<pre_arr.length; k3++) {
-                        pre_genes[k3] = pre_arr[k3].text;
-                    }
+                    var ortho_names = Object.keys(ortho_data);
+                    var ortho_scores = Object.values(ortho_data);
                     // tagname (e.g., 'name') is the name of one of the tags/nodes of a 'clade' node
                     var nodes = tree.getElementsByTagName(tagName);
                     for (var i=0; i<nodes.length; i++) {
                         var  node = nodes[i];
                         if (node.childNodes.length > 0) {
                             var gene_name = node.innerHTML;
+                            if (gene_name.length <= 10) { continue; }
+
                             var parnt = node.parentNode;
-                            var colr = '', score = 0;
-                            for (var j=0; j<can_genes.length; j++) {
-                                if (gene_name.indexOf(can_genes[j]) >= 0) { // found annotation in gene_name
-                                    colr = '0xFF0000';  // 'red'; default color, not in curation or prediction
-                                    if (pre_genes.includes(can_genes[j])) colr = '0x0000FF';  // 'blue'; present in pre_genes
-                                    else if (cur_genes.includes(can_genes[j])) colr = '0x00FF00';  // 'green'; present in cur_genes
-                                    // Because `sepeciations` can only take nonNegativeInteger values, use toFixed(2)*100 number
-                                    score = Number(can_arr[j]['value']).toFixed(2)*100;
+                            var colr = '', score = 0.0, colorR = 0, colorG = 0, colorB = 0;
+                            gene_name = gene_name.split('||')[1];
+                            for (var j=0; j<ortho_names.length; j++) {
+                                var ortho_nm = ortho_names[j].split('||')[1];
+                                if (ortho_nm.indexOf(gene_name) >= 0) { // found ortholog name in tree gene name
+                                    var score = ortho_scores[j] * 100;
+                                    // normalizing the RGB for the range of score (25%~75%)
+                                    [colorR, colorG, colorB] = get_rgb(25, 75, score);
+                                    colr = 'rgb(' + colorR + ',' + colorG + ',' + colorB + ')';
                                     break;
                                 }
                             }
